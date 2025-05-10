68f09df0ca8f7cc30d4d66161c2a55d0b2ac2254
kaylajlu <kaylajlu@gmail.com>
2025-05-10 21:38:19 +0000

    Deploying to gh-pages from @ kaylajlu/kaylajlu.github.io@0ba61958d580b64917463fb2fbecba528a185033 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Some cool things I have worked on.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
