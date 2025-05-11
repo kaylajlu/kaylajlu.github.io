755d7f1af5fbe1321274cd74a8be004610b35dc4
kaylajlu <kaylajlu@gmail.com>
2025-05-11 03:36:36 +0000

    Deploying to gh-pages from @ kaylajlu/kaylajlu.github.io@f1278ea3ddcf09d5dfea39cc61ecf3229a3b0669 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,12 +45,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-announcement-1",
+      },{id: "news-announcement-1",
           title: 'Announcement_1',
           description: "",
           section: "News",handler: () => {
